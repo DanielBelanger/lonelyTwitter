d2f36064173aa6bdb3a263b71ea1bd7e483362e3
DanielBelanger <d.e.belanger80@gmail.com>
2015-10-14 18:55:52 -0600

    Added more tests and EditTweetActivity

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,7 +1,12 @@
 package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import junit.framework.TestCase;
 
@@ -10,8 +15,84 @@ import junit.framework.TestCase;
  */
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
 
+    private EditText bodyText;
+    private Button saveButton;
+
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void testaddTweet(){
+        LonelyTwitterActivity activity = (LonelyTwitterActivity)getActivity();
+        bodyText = activity.getBodyText();
+        activity.runOnUiThread(new Runnable() {
+
+            public void run() {
+                bodyText.setText("hello");
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+
+        saveButton = activity.getSaveButton();
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                saveButton.performClick();
+            }
+        });
+
+        getInstrumentation().waitForIdleSync();
+
+        //make sure the tweet was actually added
+        final ListView oldTweetsList = activity.getOldTweetsList();
+        Tweet tweet = (Tweet) oldTweetsList.getItemAtPosition(0);
+        assertEquals("hello", tweet.getText());
+
+
+
+        //ensure the tweet editor activity starts up
+        //following from https://developer.android.com/training/activity-testing/activity-functional-testing.html 14-10-2015
+        // Set up an ActivityMonitor
+        Instrumentation.ActivityMonitor receiverActivityMonitor =
+                getInstrumentation().addMonitor(EditTweetActivity.class.getName(),
+                        null, false);
+
+
+        //the code where they clicked was here
+        //click on the tweet to edit
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                View v = oldTweetsList.getChildAt(0);
+                oldTweetsList.performItemClick(v, 0, v.getId());
+            }
+        });
+        getInstrumentation().waitForIdleSync();
+        // Validate that ReceiverActivity is started
+        EditTweetActivity receiverActivity = (EditTweetActivity)
+                receiverActivityMonitor.waitForActivityWithTimeout(1000);
+        assertNotNull("ReceiverActivity is null", receiverActivity);
+        assertEquals("Monitor for ReceiverActivity has not been called",
+                1, receiverActivityMonitor.getHits());
+        assertEquals("Activity is of wrong type",
+                EditTweetActivity.class, receiverActivity.getClass());
+
+        // Remove the ActivityMonitor
+        getInstrumentation().removeMonitor(receiverActivityMonitor);
+
+        //test that the editor starts up with te right tweet in it to edit
+        Tweet tweet = (tweet) oldTweetsList.getItemAtPosition(0);
+        assertEquals(tweet, editBodytext);
+
+        //test that we can edit that tweet
+
+        //test that we can push some kind of save buton for that tweet
+
+        //the new modified tweet text was actutally saved
+
+        //the new modified tweet text is displayed on the other activity
+
+        //clean up our acivities at the end of our tes
+        receiverActivity.finish();
     }
 
     public void testStart() throws Exception {
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -14,8 +14,10 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,8 +29,30 @@ import com.google.gson.reflect.TypeToken;
 public class LonelyTwitterActivity extends Activity {
 
 	private static final String FILENAME = "file.sav";//model
+	private LonelyTwitterActivity activity = this;
+
+	public Button getSaveButton() {
+		return saveButton;
+	}
+
+	private Button saveButton;
+
+	public EditText getBodyText() {
+		return bodyText;
+	}
+
 	private EditText bodyText;//model
+
+	public ArrayList<Tweet> getTweets() {
+		return tweets;
+	}
+
 	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();//model
+
+	public ListView getOldTweetsList() {
+		return oldTweetsList;
+	}
+
 	private ListView oldTweetsList;//model
 	private ArrayAdapter<Tweet> adapter;//model
 
@@ -40,7 +64,7 @@ public class LonelyTwitterActivity extends Activity {
 		setContentView(R.layout.main);//view
 
 		bodyText = (EditText) findViewById(R.id.body);//controller
-		Button saveButton = (Button) findViewById(R.id.save);//model
+		saveButton = (Button) findViewById(R.id.save);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);//controller
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +79,15 @@ public class LonelyTwitterActivity extends Activity {
 
 			}
 		});
+
+		oldTweetsList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+				Intent intent = new Intent(activity, EditTweetActivity.class);
+				startActivity(intent);
+			}
+
+		});
+
 		Button clearButton = (Button) findViewById(R.id.clear);//model
 		clearButton.setOnClickListener(new View.OnClickListener() {
 
